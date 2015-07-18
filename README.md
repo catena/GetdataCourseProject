21bfdede5918f0e13b6644f9ed37f01a43d1238d
catena <vishnusuresh1729@gmail.com>
2015-07-18 06:51:31 +0530

    Getdata Course Project initial submission

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# GetdataCourseProject
-Getting and Cleaning Data Course Project
+
+# Getting and Cleaning Data Course Project
+==========================================
+
+
+## Overview
+===========
+
+The purpose of this project is to clean the [Human Activity Recognition database][1] built from the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors.
+
+The `run_analysis.R` merges the train and test data from the UCI dataset and extracts only the measurements on the mean and standard deviation for each measurement.
+
+The tidy data will be created as two indendepent datasets:  
+    1. The measurements on the mean and standard deviation for each feature, `activityData.txt`  
+    2. Averages values per subject per activity for the dataset's mean and standard deviation features, `averageActivity.txt`
+	
+
+## Installation and Usage
+=========================
+
+This setup has been tested on Ubuntu 14.04, R 3.0.2 environment. **Requires** `data.table` package.
+
+1. Download and extract the [dataset][2]. It should result in `UCI HAR Dataset` folder that has all the required files.
+2. Change working directory to `UCI HAR Dataset` folder.
+3. Clone this repository.
+4. To generate the dataset, run 
+
+        Rscript <path to run_analysis.R>/run_analysis.R  
+        
+5. The tidy data will be created in the current folder as `activityData.txt` and `averageActivity.txt`.
+
+[1]: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+[2]: http://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
+
---
+++ run_analysis.R
@@ -0,0 +1,119 @@
+
+# read table from a file and returns as a data.table object
+reader <- function(file) {
+    records <- read.table(file, nrows = 10, stringsAsFactors = F)
+    records.classes <- sapply(records, class)
+    records <- read.table(file, colClasses = records.classes, comment.char = "")
+    records <- as.data.table(records)
+    records
+}
+
+# filter table and extract only the columns specified in subset (numeric vector)
+colFilter <- function(DT, subset) {
+    deleteCols <- setdiff(1:ncol(DT), subset)
+    DT[, (deleteCols) := NULL]
+    DT
+}
+
+# replace src column values with target column values, linked to each
+# other by the target_key and src_key
+colSubstitute <- function(target, target_key, src, src_key) {
+    setkeyv(target, target_key)
+    target[J(src[[src_key]])]
+}
+
+# create a reader that substitutes activity identifiers with activity names
+makeActivityLabelReader <- function(activity_list_file) {
+    activity_list <- reader(activity_list_file)
+    
+    function(activity_id_file) {
+        activity_id <- reader(activity_id_file)
+        activity_id <- colSubstitute(activity_list, "V1", activity_id, "V1")
+        colFilter(activity_id, 2)
+    }
+}
+
+# creates a reader that extracts the required mean and standard deviation
+# features from the feature vector
+makeFeatureVectorReader <- function(features_file) {
+    features <- reader(features_file)
+    
+    # find the indices/names of required features to preserve
+    feature_idx <- grep("mean\\(\\)|std\\(\\)|meanFreq\\(\\)", 
+                        features[[2]], ignore.case = T)
+    
+    # remove parantheses in feature names
+    feature_names <- features[feature_idx][[2]]
+    feature_names <- gsub("\\(\\)", "", feature_names)
+
+    function(feature_vector_file) {
+        records <- reader(feature_vector_file)
+        colFilter(records, feature_idx)     # remove unwanted features
+        setnames(records, feature_names)
+        records
+    }
+}
+
+# build the activity dataset
+buildActivityData <- function() {
+    trainFiles <- list(feature_vector = "train/X_train.txt",
+                       activity_id = "train/y_train.txt",
+                       subject_id = "train/subject_train.txt")
+    testFiles <- list(feature_vector = "test/X_test.txt",
+                      activity_id = "test/y_test.txt",
+                      subject_id = "test/subject_test.txt")
+    
+    activityLabelRead <- makeActivityLabelReader("activity_labels.txt")
+    featureVectorRead <- makeFeatureVectorReader("features.txt")
+    strategy <- list(activityLabelRead = activityLabelRead, 
+                     featureVectorRead = featureVectorRead)
+    
+    trainRecords <- buildActivityDataSingle(c(trainFiles, strategy))
+    testRecords <- buildActivityDataSingle(c(testFiles, strategy))
+    
+    # merge train and test records
+    records <- rbindlist(list(trainRecords, testRecords))
+    setorderv(records, c("subjectId", "activityName"))
+}
+
+buildActivityDataSingle <- function(context) {
+    subjectId <- reader(context$subject_id)
+    activityName <- context$activityLabelRead(context$activity_id)
+    records <- context$featureVectorRead(context$feature_vector)
+    
+    # add subjectId and activityName columns
+    records[, subjectId := subjectId][, activityName := activityName]
+    
+    # move subjectId and activityName to the front
+    n <- ncol(records)
+    setcolorder(records, c(n - 1, n, seq_len(n - 2)))
+    
+    records
+}
+
+# build average activity data, mean of all features grouped by subjectId
+# and activityName
+buildMeanActivityData <- function(feature_data) {
+    avgFeature <- feature_data[, lapply(.SD, mean), 
+                               by = list(subjectId, activityName)]
+    setorderv(avgFeature, c("subjectId", "activityName"))
+}
+
+# persist data.table to disk
+write.dataset <- function(dt, file) {
+    write.table(dt, file = file, quote = FALSE, sep = "\t",
+                row.names = FALSE, col.names = TRUE)
+}
+
+# create the datasets, driver method
+createDatasets <- function() {
+    featureData <- buildActivityData()
+    meanFeatureData <- buildMeanActivityData(featureData)
+    write.dataset(featureData, "./activityData.txt")
+    write.dataset(meanFeatureData, "./averageActivity.txt")
+}
+
+library(data.table)
+createDatasets()
+
+
